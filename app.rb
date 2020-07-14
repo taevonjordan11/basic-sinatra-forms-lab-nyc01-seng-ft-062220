3c62822bdefba6d9688dc7ed8ce9d0056e59147b
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-14 18:10:18 -0400

    done. not sure why im not passin deliverables

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = params[:team_name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sf = params[:sf]
+    @c = params[:c]
+    @sg = params[:sg]
+    @pf = params[:pf]
+
+    erb :team
+  end
+
+
 
 end
